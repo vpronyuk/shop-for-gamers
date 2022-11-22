f3b5893802e6c9245a46d1fd90cd41ffd7abc0db
Andrii-Semeniv <andrii.semeniv7@gmail.com>
2022-11-22 23:28:40 +0200

    modal-menu-icon

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,16 +1,20 @@
 (() => {
+  const mobBtn = 'data-menu-mobBtn';
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
     body: document.querySelector('body'),
+    mobBtn: document.querySelector(['data-menu-mobBtn']),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.mobBtn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
     refs.body.classList.toggle('no-scroll');
+    refs.mobBtn.classList.toggle('is-hidden');
   }
 })();
